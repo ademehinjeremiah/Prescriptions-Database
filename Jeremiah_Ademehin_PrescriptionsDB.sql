134db2f752e8cb575b54bd7af36aa312cab2e0e7
ademehinjeremiah <124537084+ademehinjeremiah@users.noreply.github.com>
2023-05-14 16:47:20 +0100

    Update Jeremiah_Ademehin_PrescriptionsDB.sql

--- Jeremiah_Ademehin_PrescriptionsDB.sql
+++ Jeremiah_Ademehin_PrescriptionsDB.sql
@@ -1,5 +1,5 @@
 
---1
+
 --Create the database
 CREATE DATABASE PrescriptionsDB;
 USE PrescriptionsDB;
@@ -27,23 +27,23 @@ ADD CONSTRAINT FK_Prescriptions_Medical_Practice FOREIGN KEY (PRACTICE_CODE)
 REFERENCES Medical_Practice(PRACTICE_CODE)
 
 
---2
+--query that returns details of all drugs which are in the form of tablets or capsules.
 
 SELECT *
 FROM Drugs
 WHERE BNF_DESCRIPTION LIKE '%tablet%' OR BNF_DESCRIPTION LIKE '%capsule%'
 
 
---3
+--query that returns the total quantity for each of prescriptions
 SELECT p.PRESCRIPTION_CODE, ROUND(CAST(p.QUANTITY * p.ITEMS AS FLOAT), 0) AS TOTAL_QUANTITY
 FROM Prescriptions p;
 
---4
+--query that returns a list of the distinct chemical substances which appear inthe Drugs table
 
 SELECT DISTINCT CHEMICAL_SUBSTANCE_BNF_DESCR
 FROM Drugs
 
---5
+--query that returns the number of prescriptions for eachBNF_CHAPTER_PLUS_CODE, along with the average cost for that chapter code, and theminimum and maximum prescription costs for that chapter code
 
 SELECT d.BNF_CHAPTER_PLUS_CODE, 
 COUNT(p.PRESCRIPTION_CODE) AS num_prescriptions, 
@@ -54,7 +54,7 @@ FROM Drugs d
 INNER JOIN Prescriptions p ON d.BNF_CODE = p.BNF_CODE 
 GROUP BY d.BNF_CHAPTER_PLUS_CODE 
 
---6
+--query that returns the most expensive prescription prescribed by eachpractice, sorted in descending order by prescription cost
 
 SELECT mp.PRACTICE_NAME, p.ACTUAL_COST
 FROM Medical_practice mp
@@ -68,9 +68,7 @@ GROUP BY mp.PRACTICE_NAME, p.ACTUAL_COST
 HAVING p.ACTUAL_COST > 4000
 ORDER BY p.ACTUAL_COST DESC
 
---7
 
---A
 --Retrieving data of medical practices that have prescribed inhalers.
 SELECT PRACTICE_NAME, ADDRESS_1, POSTCODE
 FROM Medical_practice
@@ -82,14 +80,14 @@ WHERE EXISTS (
     AND Prescriptions.PRACTICE_CODE = Medical_practice.PRACTICE_CODE
 )
 
---B
+
 --Average character count of the PRACTICE_NAME column in the Medical_practice table
 SELECT 
   AVG(LEN(PRACTICE_NAME)) AS AVG_CHAR_COUNT
 FROM 
   Medical_practice;
 
---C
+
 --The number  and the average cost of prescriptions for each medical practice that has prescribed more than 50 prescriptions in descending order of average cost of prescriptions
 SELECT 
     p.PRACTICE_NAME,
@@ -105,7 +103,7 @@ HAVING
 ORDER BY 
     AVG_COST DESC;
 
---D
+
 -- Medical practices with more than 500 prescriptions and the total number of prescriptions made by each practice.
 
 SELECT 
@@ -121,7 +119,7 @@ HAVING
 ORDER BY 
   TOTAL_PRESCRIPTIONS DESC;
 
---E
+
 --Top 10 medical practices that have the highest average cost per prescription
 SELECT TOP 10
     p.PRACTICE_NAME,
